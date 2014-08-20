e661b13c99bf7e64b825086a01551759b1da294e
Franklin Valadares <valadares@gmail.com>
2014-08-20 18:57:54 -0300

    Add get_goals_per_user

--- lib/pipedrive/goal.rb
+++ lib/pipedrive/goal.rb
@@ -5,5 +5,9 @@ module Pipedrive
       Goal.all(get "#{resource_path}/goals?everyone=1")
     end
 
+    def goals_per_user(user_id)
+      Goal.all(get "#{resource_path}/goals?user_id=#{user_id}")
+    end
+
   end
 end
